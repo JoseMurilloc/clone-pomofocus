66249e4956c3622ac30fef2786adc1e1cae93eaf
José Murillo <jooseemurillo@gmail.com>
2022-03-16 13:54:43 -0300

    fix: rename Main to Home in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import {
   Routes as RouterDom,
   Route,
 } from "react-router-dom";
-import {Main} from '../pages/Main';
+import {Home} from '../pages/Home';
 import {Login} from '../pages/Login';
 import { ForgotPassword } from "../pages/ForgotPassword";
 import { RegisterMe } from "../pages/RegisterMe";
@@ -11,7 +11,7 @@ import { RegisterMe } from "../pages/RegisterMe";
 export function Routes() {
   return (
     <RouterDom>
-      <Route path="/" element={<Main/>}/>
+      <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<Login/>}/>  
       <Route path="/forgot" element={<ForgotPassword/>} />
       <Route path="/register-me" element={<RegisterMe/>} />
