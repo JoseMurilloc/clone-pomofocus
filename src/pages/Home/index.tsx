da959a4b22ed7909cd7843e501245e588e352e20
José Murillo <jooseemurillo@gmail.com>
2022-04-06 08:37:20 -0300

    chore: remove div without useful

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -61,39 +61,37 @@ export function Home () {
       { visibleMenuProfile && <MenuProfile /> }
 
       <Header typePomodoro={typePomodoro}>
-        <div>
-          <h1>
-            <IconMain.FaCheckCircle color="#fff" size="2rem" />
-            Pomodoro
-          </h1>
+        <h1>
+          <IconMain.FaCheckCircle color="#fff" size="2rem" />
+          Pomodoro
+        </h1>
 
-          <ContentOfButtonsHeader typePomodoro={typePomodoro}>
-            <button className="button-default">
-              <IconMain.GoGraph size="1.8rem" color="#fff" />
-              <span>Report</span>
-            </button>
-            <button className="button-default">
-              <IconMain.MdSettings size="1.8rem" color="#fff" />
-              <span>Setting</span>
-            </button>
+        <ContentOfButtonsHeader typePomodoro={typePomodoro}>
+          <button className="button-default">
+            <IconMain.GoGraph size="1.8rem" color="#fff" />
+            <span>Report</span>
+          </button>
+          <button className="button-default">
+            <IconMain.MdSettings size="1.8rem" color="#fff" />
+            <span>Setting</span>
+          </button>
 
-            { false ? (
-              <div className="login-user">
-                <img
-                  src="https://lh3.googleusercontent.com/a-/AOh14Ggsx2eT_A6emvGukBj5QrwRCqyJNSxD-5j5FvFLpw=s96-c"
-                  alt="profile"
-                />
-              </div>
-            ) : (
-              <Link to="/login">
-                <button className="button-default">
-                  <IconMain.FaUserCircle size="1.8rem" color="#fff" />
-                  <span>Login</span>
-                </button>
-              </Link>
-            ) }
-          </ContentOfButtonsHeader>
-        </div>
+          { false ? (
+            <div className="login-user">
+              <img
+                src="https://lh3.googleusercontent.com/a-/AOh14Ggsx2eT_A6emvGukBj5QrwRCqyJNSxD-5j5FvFLpw=s96-c"
+                alt="profile"
+              />
+            </div>
+          ) : (
+            <Link to="/login">
+              <button className="button-default">
+                <IconMain.FaUserCircle size="1.8rem" color="#fff" />
+                <span>Login</span>
+              </button>
+            </Link>
+          ) }
+        </ContentOfButtonsHeader>
       </Header>
       <Main>
         <Time typePomodoro={typePomodoro}>
