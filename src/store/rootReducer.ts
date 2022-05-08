346f477f21e79492c05b9563867ebe0aba1b9454
José Murillo <jooseemurillo@gmail.com>
2022-05-08 12:38:04 -0300

    chore: add task in root reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import modal from './modules/modal/reducer';
+import task from './modules/task/reducer';
 
 const reducers = combineReducers({
   modal,
+  task
 });
 
 export default reducers;
