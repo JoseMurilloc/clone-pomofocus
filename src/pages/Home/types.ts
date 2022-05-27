991618d02ff972db5dbabb702a769823862ec1b2
José Murillo <jose.nunes@pede.ai>
2022-05-27 17:05:38 -0300

    create type Task to home page

--- src/pages/Home/types.ts
+++ src/pages/Home/types.ts
@@ -2,9 +2,20 @@ import { Status } from "../../commons/types/status"
 
 export type typeProps = {
   typePomodoro: Status;
-} 
+}
 
 export type Option = {
   status: Status;
   label: string;
 }
+
+export type Task = {
+  title: string
+  est: {
+    current: number;
+    final: number;
+  };
+  note?: string;
+  finally: boolean;
+}
+
