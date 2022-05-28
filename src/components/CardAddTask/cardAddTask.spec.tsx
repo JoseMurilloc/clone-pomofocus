0efc0d2b0e500f583144eca4da41ad56621b2a31
José Murillo <jooseemurillo@gmail.com>
2022-05-28 12:21:28 -0300

    test: clean form after submit new new task

--- src/components/CardAddTask/cardAddTask.spec.tsx
+++ src/components/CardAddTask/cardAddTask.spec.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { CardAddTask } from '.';
 import userEvent from '@testing-library/user-event';
+import { Providers } from '../../utils/providersTest';
 
 describe('<CardAddTask />', () => {
+
   it('should be able of close card add task on click and cancel', () => {
     const onVisibleMock = jest.fn()
 
-    render(<CardAddTask onVisible={onVisibleMock}/>)
+    render(<CardAddTask onVisible={onVisibleMock}/>, { wrapper: Providers })
 
     const buttonCancel = screen.getByRole('button', {
       name: /Cancel/
@@ -21,7 +23,7 @@ describe('<CardAddTask />', () => {
   it('should render and show correctly note resource', () => {
     const onVisibleMock = jest.fn()
 
-    render(<CardAddTask onVisible={onVisibleMock}/>)
+    render(<CardAddTask onVisible={onVisibleMock}/>, { wrapper: Providers })
 
     const buttonAddResource = screen.getByRole('button', {
       name: "+ Add project"
@@ -34,7 +36,7 @@ describe('<CardAddTask />', () => {
   it('should be able on click in addResource show in page note resource', () => {
     const onVisibleMock = jest.fn()
 
-    render(<CardAddTask onVisible={onVisibleMock}/>)
+    render(<CardAddTask onVisible={onVisibleMock}/>, { wrapper: Providers })
 
     const buttonAddNote = screen.getByRole('button', {
       name: "+ Add note"
@@ -51,7 +53,7 @@ describe('<CardAddTask />', () => {
   it('should not be able decrement counter with number negative', () => {
     const onVisibleMock = jest.fn()
 
-    render(<CardAddTask onVisible={onVisibleMock}/>)
+    render(<CardAddTask onVisible={onVisibleMock}/>, { wrapper: Providers })
 
     const buttonDecrementElement = screen.getByTestId('button-decrement')
 
@@ -66,7 +68,7 @@ describe('<CardAddTask />', () => {
   it('should be able increment counter est', () => {
     const onVisibleMock = jest.fn()
 
-    render(<CardAddTask onVisible={onVisibleMock}/>)
+    render(<CardAddTask onVisible={onVisibleMock}/>, { wrapper: Providers })
 
     const buttonIncrementElement = screen.getByTestId('button-increment')
 
@@ -76,7 +78,47 @@ describe('<CardAddTask />', () => {
 
     const inputCounterElement =  screen.getByTestId('input-counter')
 
-    expect(inputCounterElement).toHaveValue(3)
+    expect(inputCounterElement).toHaveValue(4)
+  })
+
+  it('should be able clean form after submit new task success', async () => {
+    const onVisibleMock = jest.fn()
+
+    render(<CardAddTask onVisible={onVisibleMock}/>, { wrapper: Providers })
+
+    const buttonSaveTask = screen.getByRole('button', {
+      name: /Save/
+    })
+
+    const buttonAddNote = screen.getByRole('button', {
+      name: "+ Add note"
+    })
+
+    const buttonIncrement = screen.getByTestId('button-increment')
+
+    const inputTitle = screen.getByPlaceholderText('What are your working on?')
+    const inputEst = screen.getByTestId('input-counter');
+
+
+    await userEvent.type(inputTitle, 'Initial task')
+
+    userEvent.click(buttonAddNote)
+    userEvent.click(buttonIncrement)
+
+    const inputNote = screen.getByTestId('note-est-pomodoro');
+
+    await userEvent.type(inputNote, 'Lorem ipsum')
+
+    userEvent.click(buttonSaveTask)
+
+    await waitFor(() => {
+      expect(inputTitle).toHaveValue('')
+    })
+
+    await waitFor(() => {
+      expect(inputNote).toHaveValue('')
+    })
+    expect(inputEst).toHaveValue(1)
   })
 })
 
