3bc4fa0d5edfcf02807131f57ee7f6aaa4369303
José Murillo <jooseemurillo@gmail.com>
2022-05-28 12:28:36 -0300

    feature: select task with toggle status of redux

--- src/components/CardTask/index.tsx
+++ src/components/CardTask/index.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import { Task } from '../../store/modules/task/types';
 import { FaCheckCircle } from 'react-icons/fa';
 import {GrMoreVertical} from 'react-icons/gr';
+import { useDispatch } from 'react-redux';
+import { toggleAStatusTaskAction } from '../../store/modules/task/actions';
 import * as S from './styles'
+import { Props } from './types';
 
-type Props = {
-  task: Task
-}
 
-export function CardTask({task}: Props) {
+export function CardTask({task, selected, onSelectedTask}: Props) {
+  const dispatch = useDispatch()
   return (
-    <S.Container>
-      <S.Header>
+    <S.Container
+      data-testid="card-task"
+      selectedCard={selected}
+      onClick={() => onSelectedTask(task.title)}
+    >
+      <S.Header completeTask={task.finally}>
         <div className="title">
-          <FaCheckCircle color="#dfdfdf" size={25} />
+          <button
+            onClick={() => dispatch(toggleAStatusTaskAction(task.title))}
+          >
+            <FaCheckCircle
+              color={task.finally ? '#D95550' : '#dfdfdf'}
+              size={25}
+            />
+          </button>
           <span>{task.title}</span>
+
         </div>
         <S.EstSetting>
           <div className="est">
